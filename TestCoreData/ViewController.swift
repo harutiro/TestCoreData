642ea403a12bdc171b79bc04142681898d2daaf3
harutiro <harutiro2027@outlook.jp>
2023-03-08 15:47:42 +0900

    Initial Commit

---
+++ TestCoreData/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestCoreData
+//
+//  Created by haruto.makino on 2023/03/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
